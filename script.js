d62701360668363974b8aa2d20f68e65b6ea13d3
CapacitorSet <capacitorset@gmail.com>
2021-01-06 22:56:10 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function onChangeRegione() {
+    var regione = document.getElementById("regione").value;
+    window.location = "/" + regione + ".html";
+}
+function svg_open(regione) {
+    window.location = "/" + regione + ".html";
+}
